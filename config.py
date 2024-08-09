0d58bcf64bdc91fada3888132002a459122ae9f8
Byerror <153293815+Byerror@users.noreply.github.com>
2024-08-09 18:41:05 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,81 @@
+
+CHANNEL_ID = -1002129853604  # Айди канала
+INVOICE_LINK = "t.me/send?start=IVrAV6WWx3WV"  #  Линк на инвойс
+RULES_LINK = "https://t.me/igriwa"  #  Линк на правила
+NEWS_LINK = "https://t.me/+H-fgHW_j4pUwN2Ey"  #  Линк польз. соглашения
+SUPPORT_LINK = "https://t.me/SC999AM"  #  Линк саппорта
+FAQ_LINK = "https://t.me/igriwa" # FAQ
+CRYPTO_PAY_TOKEN = "244808:AAgSdAyAbLUcSYkF8nJ8tZUP9GeYe3lEKrB"  #  Токен от CryptoPay фулл откат сделал, ща посмотрим
+BOT_TOKEN = "7405917761:AAGAkjiyPaLPRVrVzWPQQAymv11eAZkifi0"  #  Токен бота че тупой, не я вставлял ранее, я говорю фулл скрипт откатитл, до изменений
+SCAM_SUM = 5
+SCAM_SUM_WHEEL = 10
+SCAM_WHEEL = 0
+ADMIN_ID = 6584286070 # Айди админа
+ADMIN_SD = 6584286070
+NEWS_ID = -1002181538082  # Айди канала новостей
+VIPLAT_ID = -1002195516136
+min_vivod = 1.15
+xamount_action = False
+kef = 2.8
+kef2 = 1.8
+kefzalupa = 1.85
+kefsuefa = 2.5
+kefbasket = 1.3
+max_actions = 4
+timeout = 20
+wheelscum = 0
+kef_kub = 1.85
+kef_suefa = 2.5
+WIN_IMAGE = 'http://i.imgur.com/MqD59nL.jpg'
+LOSE_IMG = 'http://i.imgur.com/PmfLrpB.jpg'
+CHAT_LINK = "https://t.me/+OzXsd7jXeT9hMGUy"
+CHAT_MESSAGE_INTERVAL = 3000
+OFFLINE_DOLLAR = 92
+ERROR_CHAT_ID = -1002195516136
+MAX_BET = 100
+
+bowling_values = {
+    1: 6,
+    2: 5,
+    3: 3,
+    4: 2, 
+    5: 1,
+    6: 0
+}
+
+wheel = {
+    "green": {"coef": 15,"probability": 0.01},
+    "black": {"coef": 2,"probability": 0.495},
+    "red": {"coef": 2,"probability": 0.495}
+}
+
+
+crush = [0.60,0.39,0.01]
+
+
+knb = {
+    "камень": "🖐",
+    "бумага": "✌️",
+    "ножницы": "✊",
+}
+
+
+knb_win = {
+    "камень": "✌️",
+    "бумага": "✊",
+    "ножницы": "🖐",
+}
+
+
+knb_values = {
+    "камень": "✊",
+    "бумага": "🖐",
+    "ножницы": "✌️",
+}
+
+
+knb_smiles = {
+    "✊": "камень",
+    "🖐": "бумага",
+    "✌️": "ножницы",
+}
